bc35838945eb3ceb72e5b4cf49b84d9df7f4521b
Habib5634 <hero5276311@gmail.com>
2025-04-05 14:23:41 +0500

    done

--- src/utils/ApiUrl.js
+++ src/utils/ApiUrl.js
@@ -1,7 +1,7 @@
 // import { useSelector } from "react-redux";
 
 // export const API_URL = 'https://virtual-arena-backend.vercel.app/api/v1';
-export const API_URL = 'http://localhost:8080/api/v1';
+export const API_URL = 'https://virtual-arena-backend.vercel.app/api/v1';
 export const SOCKET_URL = 'http://localhost:5000/';
 // export const useAuthToken = () => {
 //     return useSelector((state) => state.login.token);
